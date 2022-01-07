34532371e1f9e2b404ddaa62eb6fc2cb4541d24d
teacmel <97210344+teacmel@users.noreply.github.com>
2022-01-08 01:16:41 +0800

    a

---
+++ propertytask1.sql
@@ -0,0 +1,24 @@
+﻿--a. Display a list of all property names and their property id’s for Owner Id: 1426. 
+SELECT DISTINCT 
+[OwnerProperty].[OwnerId],
+[Property].[Id],
+[Property].[Name] AS PropertyName
+FROM [MVPSTUDIO.CDVKL5VM8WEQ.AP-SOUTHEAST-2.RDS.AMAZONAWS.COM].[Keys].[dbo].[Property]
+JOIN [MVPSTUDIO.CDVKL5VM8WEQ.AP-SOUTHEAST-2.RDS.AMAZONAWS.COM].[Keys].[dbo].[OwnerProperty]
+ON [OwnerProperty].[OwnerId]  = 1426
+ORDER BY PropertyName
+
+--b.	Display the current home value for each property in question a). 
+SELECT DISTINCT
+[OwnerProperty].[OwnerId],
+[Property].[Id],
+[PropertyHomeValue].[IsActive],
+[Property].[Name] AS PropertyName,
+[PropertyHomeValue].[Value] AS PropertyCurrentValue
+FROM [MVPSTUDIO.CDVKL5VM8WEQ.AP-SOUTHEAST-2.RDS.AMAZONAWS.COM].[Keys].[dbo].[Property]
+JOIN [MVPSTUDIO.CDVKL5VM8WEQ.AP-SOUTHEAST-2.RDS.AMAZONAWS.COM].[Keys].[dbo].[PropertyHomeValue]
+ON [PropertyHomeValue].[PropertyId] = [Property].[Id] 
+JOIN [MVPSTUDIO.CDVKL5VM8WEQ.AP-SOUTHEAST-2.RDS.AMAZONAWS.COM].[Keys].[dbo].[OwnerProperty]
+ON [OwnerProperty].[OwnerId]  = 1426 
+WHERE [PropertyHomeValue].[IsActive] = '1'
+ORDER BY [Property].[Id]
