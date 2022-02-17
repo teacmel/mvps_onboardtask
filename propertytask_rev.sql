0d10621ff9daa1f1430bb1edf7343e95bb4d725a
Melvin Yee <97210344+teacmel@users.noreply.github.com>
2022-02-17 17:19:26 +0800

    update 170222

--- propertytask_rev.sql
+++ propertytask_rev.sql
@@ -21,55 +21,69 @@ INNER JOIN dbo.[PropertyHomeValue] phv
 ON phv.[PropertyId] = p.[Id] 
 INNER JOIN dbo.[OwnerProperty] op
 ON op.[PropertyId]  = p.[Id]
-WHERE op.[OwnerId]  = 1426
+WHERE op.[OwnerId]  = 1426 and
+phv.[IsActive] = '1'
 
 /*c.For each property in question a), return the following:                                                                      
-i.	Using rental payment amount, rental payment frequency, tenant start date and tenant end date to write a query that returns the sum of all payments from start date to end date. 
+i.	Using rental payment amount, rental payment frequency, tenant start date and tenant end date to write 
+a query that returns the sum of all payments from start date to end date. 
 */
-SELECT 
+SELECT DISTINCT
 p.[Id] as 'Property ID',
 p.[Name] as 'Property Name',
-tp.[PaymentAmount],
+prep.[Amount],
 tp.[StartDate] as 'Tenant Start Date',
 tp.[EndDate] as 'Tenant End Date',
-trt.[Name] as 'Rental Payment Frequency',
 
 CASE
-WHEN FrequencyType = '1' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate]) * tp.[PaymentAmount]  
-WHEN FrequencyType = '2' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate]) * (tp.[PaymentAmount]/2)
-WHEN FrequencyType = '3' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate])*12*(tp.[PaymentAmount])/52
-END as "Sum of all Payments"
+WHEN FrequencyType = '1' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate]) * prep.[Amount]  
+WHEN FrequencyType = '2' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate]) * (prep.[Amount]/2)
+WHEN FrequencyType = '3' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate])*12*(prep.[Amount])/52
+END as "Sum of all Payments",
 
-FROM dbo.[PropertyRentalPayment] prp
+CASE
+WHEN p.[TargetRentTypeId] = '1' THEN ('Weekly')
+WHEN p.[TargetRentTypeId] = '2' THEN ('Fortnightly')
+WHEN p.[TargetRentTypeId] = '3' THEN ('Monthly')
+END as 'Rental Payment Frequency'
+
+FROM dbo.[Property] p
+INNER JOIN dbo.[OwnerProperty] op
+ON p.[Id] = op.[PropertyId] 
+INNER JOIN dbo.[PropertyRepayment] prep
+ON op.[PropertyId]  = prep.[PropertyId]
 INNER JOIN dbo.[TenantProperty] tp
-ON tp.[PropertyId] = prp.[PropertyId]
-INNER JOIN dbo.[Property] p
-ON p.[Id] = prp.[PropertyId]
-FULL JOIN dbo.[TargetRentType] trt
-ON trt.[Id] = prp.[FrequencyType]
-WHERE tp.[PropertyId] IN ('5597', '5637', '5638')
+ON tp.[PropertyId] = prep.[PropertyId]
+WHERE op.[OwnerId]  = 1426
 ORDER BY p.[Id]
 
  /*c.For each property in question a), return the following:                                                                      
-ii.	Display the yield. */
-WITH temptable
+ii.	Display the yield. 
+*/
+WITH #t1
 as (
-SELECT tp.[PropertyId], 
-(CASE
-WHEN tp.[PaymentFrequencyId] = '1' THEN (DATEDIFF(Week,tp.[StartDate],tp.[EndDate])*tp.[PaymentAmount])
-WHEN tp.[PaymentFrequencyId] = '2' THEN (DATEDIFF(Week,tp.[StartDate],tp.[EndDate])*tp.[PaymentAmount]/2)
-WHEN tp.[PaymentFrequencyId] = '3' THEN (DATEDIFF(Week,tp.[StartDate],tp.[EndDate])*12*tp.[PaymentAmount]/52)
-END) as TRP, COALESCE(PE.Amount,0) as Amount, pf.[CurrentHomeValue]
-FROM dbo.[TenantProperty] as tp
-LEFT JOIN dbo.[PropertyExpense] as pe ON tp.PropertyId = pe.PropertyId
-LEFT JOIN dbo.[PropertyFinance] as pf ON tp.PropertyId = pf.PropertyId
+SELECT prep.[PropertyId], 
+CASE
+WHEN FrequencyType = '1' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate]) * prep.[Amount]  
+WHEN FrequencyType = '2' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate]) * (prep.[Amount]/2)
+WHEN FrequencyType = '3' THEN DATEDIFF(WEEK,tp.[StartDate],tp.[EndDate])*12*(prep.[Amount])/52
+END as TRP, COALESCE(PE.Amount,0) as Expense, 
+phv.[Value]
+FROM dbo.[tenantProperty] tp
+LEFT JOIN dbo.[PropertyRepayment] as prep ON prep.PropertyId = tp.PropertyId
+LEFT JOIN dbo.[PropertyExpense] as pe ON pe.PropertyId = tp.PropertyId
+LEFT JOIN dbo.[PropertyHomeValue] as phv ON tp.PropertyId = phv.PropertyId
+WHERE phv.IsActive = '1'
     )
-SELECT [PropertyId], (TRP-[Amount])/[CurrentHomeValue]*100 as Yield
-FROM temptable
-WHERE [PropertyId] IN ('5597', '5637', '5638')
+SELECT DISTINCT op.[PropertyId], (TRP-[Expense])/[Value]*100 as Yield
+FROM #t1
+INNER JOIN dbo.[OwnerProperty] op
+ON op.[PropertyId] = #t1.PropertyId
+WHERE op.[OwnerId]  = 1426
+ORDER by [PropertyId]
 
 --d.	Display all the jobs available
-SELECT
+SELECT DISTINCT
 jm.[JobId], 
 jm.[PropertyId], 
 j.[JobDescription],
@@ -79,7 +93,7 @@ js.[Status]
 FROM dbo.[Job] j
 INNER JOIN dbo.[JobStatus] js
 ON j.[JobStatusId] = js.[Id]
-INNER JOIN dbo.[JobMedia] jm
+INNER JOIN dbo.[JobMedia] jm 
 ON j.[Id] = jm.[JobId]
 WHERE jm.[IsActive] = 1 
 AND js.[Status] = 'Open'
